3de925a6b2645a79a10ef82f99ea9367796af121
TuPpKaM <mosaren@hotmail.com>
2022-09-27 16:04:35 +0200

    preparation 1-5

--- lab2/src/lpt/TestScheduler.java
+++ lab2/src/lpt/TestScheduler.java
@@ -14,7 +14,7 @@ public class TestScheduler {
 		String [] names = {"j1", "j2", "j3", "j4", "j5", "j6", "j7"};
 		int[] times = {2, 14, 4, 16, 6, 5, 3};
 		for (int i = 0; i < names.length; i++) {
-			jobList.add(new Job(names[i], times[i]));
+			jobList.add(new Job(names[i], times[i]));	
 		}
 		
 		Scheduler s = new Scheduler(m);
