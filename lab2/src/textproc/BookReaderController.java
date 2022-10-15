a9bff10bfe2a40fcf85968be224d590cce4526ec
DESKTOP-OSFSGV9\Jeppe <mosaren@hotmail.com>
2022-10-15 17:35:46 +0200

    fixed sorting buttons, cleaned up apperance

--- lab2/src/textproc/BookReaderController.java
+++ lab2/src/textproc/BookReaderController.java
@@ -3,10 +3,14 @@ package textproc;
 import java.awt.*;
 import java.util.Map;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JList;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JRootPane;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
@@ -14,6 +18,7 @@ import javax.swing.SwingUtilities;
 import java.awt.Color;
 
 public class BookReaderController {
+        public static boolean falling = true;
         public BookReaderController(GeneralWordCounter counter) {
         SwingUtilities.invokeLater(() -> createWindow(counter, "BookReader", 1000, 500));
     }
@@ -24,52 +29,92 @@ public class BookReaderController {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setMinimumSize(new Dimension(width, height));
         Container pane = frame.getContentPane();
-        pane.setBackground(Color.DARK_GRAY);
         pane.setLayout(new BorderLayout());
 
         SortedListModel<Map.Entry<String, Integer>> wordlist = new SortedListModel<Map.Entry<String, Integer>>(counter.getWordList());
         JList<Map.Entry<String, Integer>> jlist = new JList<Map.Entry<String, Integer>>(wordlist);
         JScrollPane scrollpane = new JScrollPane(jlist);
-        scrollpane.setBackground(Color.LIGHT_GRAY);
+        scrollpane.setBackground(Color.WHITE);
         scrollpane.setPreferredSize(new Dimension(width, height-30));
         scrollpane.getVerticalScrollBar().setPreferredSize(new Dimension(30, 0));
         scrollpane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        scrollpane.setBorder(BorderFactory.createCompoundBorder(scrollpane.getBorder(), BorderFactory.createEmptyBorder(0, 10, 0, 0)));
         pane.add(scrollpane, BorderLayout.CENTER);
 
         JPanel menurow = new JPanel();
-        JButton alphabetic = makeButton("Alphabetic");
-        JButton frequency = makeButton("Frequency");
-        frequency.addActionListener(e -> wordlist.sort( (Map.Entry<String, Integer> w1, Map.Entry<String, Integer> w2) -> w2.getValue()-w1.getValue()));
-        menurow.add(alphabetic);
-        menurow.add(frequency);
-        
+        menurow.setLayout(new GridLayout(0,5));
+        JButton risingSorting = makeButton("Toggle: Sort by Falling", menurow);
+        JRadioButton alphabetic = makeRadioButton("Alphabetic", false, menurow);
+        JRadioButton frequency = makeRadioButton("Frequency", false, menurow);
         JTextField textfield = new JTextField();
-        textfield.setPreferredSize(new Dimension(300,35));
+        textfield.setPreferredSize(new Dimension(175,35));
+        textfield.setBorder(BorderFactory.createCompoundBorder(textfield.getBorder(), BorderFactory.createEmptyBorder(0, 10, 0, 0)));
         menurow.add(textfield);
-        JButton searchbtn = makeButton("Search");
-        menurow.add(searchbtn);
-        alphabetic.addActionListener(e -> wordlist.sort( (Map.Entry<String, Integer> w1, Map.Entry<String, Integer> w2) -> w1.getKey().compareTo((w2).getKey())));
+        JButton searchbtn = makeButton("Search", menurow);
+
+        risingSorting.addActionListener(e -> {
+            falling = !falling; 
+            if (falling) {
+                risingSorting.setText("Toggle: Sort by Falling:");
+            } else {
+                risingSorting.setText("Toggle: Sort by  Rising:");
+            }
+        });
+        alphabetic.addActionListener(e -> {
+            if (falling) {
+                wordlist.sort((Map.Entry<String, Integer> w1, Map.Entry<String, Integer> w2) -> w2.getKey().compareTo((w1).getKey()));
+            } else {
+                wordlist.sort((Map.Entry<String, Integer> w1, Map.Entry<String, Integer> w2) -> w1.getKey().compareTo((w2).getKey()));
+            }
+            frequency.setSelected(false);
+        });
+        frequency.addActionListener(e -> {
+            if (falling) {
+                wordlist.sort((Map.Entry<String, Integer> w1, Map.Entry<String, Integer> w2) -> w2.getValue()-w1.getValue());
+            } else {
+                wordlist.sort((Map.Entry<String, Integer> w1, Map.Entry<String, Integer> w2) -> w1.getValue()-w2.getValue());
+            }
+            alphabetic.setSelected(false);
+        });
         searchbtn.addActionListener(e -> {
+            String filteredInput;
+            try {
+                filteredInput = textfield.getText().toLowerCase().strip();
+            } catch (NullPointerException exception) {
+                JOptionPane.showMessageDialog(null, exception, "Warning", JOptionPane.ERROR_MESSAGE);
+                return;
+            }
             for (int i = 0; i<wordlist.getSize(); i++) {
-                if (wordlist.getElementAt(i).getKey().equals(textfield.getText())) {
+                textfield.setText(filteredInput);
+                if (wordlist.getElementAt(i).getKey().equals(filteredInput)) {
                     jlist.ensureIndexIsVisible(i);
                     jlist.setSelectedIndex(i);
-                    System.out.print("|"+wordlist.getElementAt(i).getKey() + "|"+textfield.getText()+ "|"+i+"|");
                     return;
                 }
             }
+            JOptionPane.showMessageDialog(null, "Can't find the word: " + textfield.getText(), "Warning", JOptionPane.ERROR_MESSAGE);
         });
 
-        pane.add(menurow, BorderLayout.SOUTH);
+        JRootPane jf = frame.getRootPane();
+        jf.setDefaultButton(searchbtn); //Enter pressses the search button
+        pane.add(menurow, BorderLayout.SOUTH); // Add menurow att the bottom
 
-        frame.pack();
+        frame.pack(); //create window
         frame.setVisible(true);
     }
 
-    private JButton makeButton(String title){
-        JButton button = new JButton();
-        button.setText(title);
+    private JButton makeButton(String title,JPanel jp){
+        JButton button = new JButton(title);
         button.setPreferredSize(new Dimension(175,35));
+        jp.add(button);
+        return button;
+    }
+
+    private JRadioButton makeRadioButton(String title, boolean selected, JPanel jp){
+        JRadioButton button = new JRadioButton(title,selected);
+        button.setPreferredSize(new Dimension(175,35));
+        button.setBorder(BorderFactory.createCompoundBorder(button.getBorder(), BorderFactory.createEmptyBorder(0, 50, 0, 0)));
+        jp.add(button);
         return button;
     }
 }
